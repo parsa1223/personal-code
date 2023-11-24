5fc9742cd9876dfd492a53917a91c2c52c862a08
Parsa Arababadi <arababadiparsa@gmail.com>
2023-11-24 19:45:20 +0330

    new shape

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 
+const selector=document.querySelector('form');
 const btn=document.querySelector('form');
-const chek=document.querySelector('#loot');
-const sub2=document.querySelector('#subb');
-
+function submitfunctoin(selector){
+const form =document.querySelector(selector)
+};
 loadEventListener();
 function loadEventListener(){
     btn.addEventListener('submit',submitsf)
@@ -10,16 +11,17 @@ function loadEventListener(){
 
 function submitsf(event){
     event.preventDefault();
-    const nameValue=btn.querySelector('[name="names"]').value;
+   document.addEventListener('DOMContentLoaded',function(){submitfunctoin('.passw')})
     
-    if( nameValue == 'parsa@gmail.com'){
+    if(    document.addEventListener('DOMContentLoaded',function(){submitfunctoin('.passw')}).value
+    == 'parsa@gmail.com'){
        submitss()
    
     }else{
    const errorr= document.getElementById('err').innerHTML="نام کاربری باید فرمت ایمیل داشته باشد"
     };
 };
-function submitss(e){
+function submitss(){
     const passLeng=btn.querySelector('[name="passw"]').value.length;
     const passValue=btn.querySelector('[name="passw"]').value;
 
@@ -37,27 +39,28 @@ function redirect(){
     window.location.href='./sec.html'
   
 };
-subb()
-function subb(){
-sub2.addEventListener('submit',chekbox)    
-}
+// subb()
+// function subb(){
+// sub2.addEventListener('submit',chekbox)    
+// }
 
-function chekbox(eve){
-eve.preventDefault();
-    const yesv=chek.querySelector('[name="qus"]');
-    if(yesv.checked ==! "true"){
-              const rrre=document.getElementById('toto').innerHTML="رمز عبور ذخیره نمیشود"
-    }else {
-        const rrre=document.getElementById('toto').innerHTML="رمز عبور ذخیره میشود"
-        store()
-    }
+// function chekbox(eve){
+// eve.preventDefault();
+//     const yesv=chek.querySelector('[name="qus"]');
+//     if(yesv.checked ==! "true"){
+//               const rrre=document.getElementById('toto').innerHTML="رمز عبور ذخیره نمیشود"
+//     }else {
+//         const rrre=document.getElementById('toto').innerHTML="رمز عبور ذخیره میشود"
+//         store()
+//     }
         
     
-};
-function store (){
-    const passValue1=btn.querySelector('.passw');
-    localStorage.setItem("password",passValue1.value);
+// };
+// function store (){
+//     const passValue=btn.querySelector('[name="passw"]');
 
-}
+//     localStorage.setItem("password",passValue);
+
+// }
 
 
