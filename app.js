6dee221733e447e84aa57db29901d055be9c3f59
Parsa Arababadi <arababadiparsa@gmail.com>
2023-12-22 22:28:08 +0330

    sesionNew

--- app.js
+++ app.js
@@ -10,17 +10,17 @@ function loadEventListener(){
 function submitsf(event){
     event.preventDefault();
   const nameValue=document.querySelector('[name="names"]').value;
-   var email={name1:'parsa@gmail.com',
+   const email={name1:'parsa@gmail.com',
    name2:'ahmad@gmail.com',
    name3 :'kianoosh@gmail.com'};
   
     
     if(   nameValue
-    == email.name1||email.name2||email.name3){
-       submitss()
+    ==! email.name1||email.name2||email.name3){
+submitss()
    
-    }else{
-   const errorr= document.getElementById('err').innerHTML="نام کاربری باید فرمت ایمیل داشته باشد"
+    }else{          const errorr= document.getElementById('err').innerHTML="نام کاربری باید فرمت ایمیل داشته باشد"
+
     };
 };
 function submitss(){
@@ -44,12 +44,10 @@ function redirect(){
 
 function checkStorageAndSendToWelcomePage(eve){
 eve.preventDefault();
-    const yesv=chek.querySelector('[name="qus"]');
+    
     if(typeof(Storage) !== "undefined"){
-    if(sessionStorage.clickcount){
-        sessionStorage.clickcount=Number(sessionStorage.clickcount)+1;
-    }else {
-    }}else{}
+        sessionStorage.setItem("islogin",1)
+    }else{}
         
     
 };
