c32a27642e19cb7cfc896fcd188e4dfea88032f5
Parsa Arababadi <arababadiparsa@gmail.com>
2024-01-07 17:53:13 +0330

    fibonacci

--- wellcom.js
+++ wellcom.js
@@ -1,9 +1,12 @@
 const khoroog=document.querySelector('#logout');
-
+const underFibon=document.querySelector('#fibonaccci')
 beforeLogout()
 
 function beforeLogout(){  
-      khoroog.addEventListener('submit',logout)};
+      khoroog.addEventListener('submit',logout)
+      underFibon.addEventListener('submit',fibonacci)
+}
+      ;
 function logout(eve){
 eve.preventDefault();   
  window.location.href='./login.html';
@@ -24,3 +27,22 @@ if(sesion ==1 || local == 1){
 
 
 };
+
+function fibonacci(e){
+      e.preventDefault()
+      const fibonacciInput=document.querySelector('#fibonache').value;
+      let n1=0 , n2=1, n3;
+
+      for( i = 1;i<=fibonacciInput;i++){
+            var result=document.getElementById('underFibonache').innerHTML=(n1);
+            n3 =n1+n2;
+            n1=n2;
+            n2=n3; 
+      }
+
+
+
+
+
+
+}
